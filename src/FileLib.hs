cea25d48315712d2537ebd00c4066116bf0d5969
ElliotKetchup <elliot.alladaye@epitech.eu>
2024-03-14 17:34:59 +0100

    doc(file_parser): document every function in FileLib

--- src/FileLib.hs
+++ src/FileLib.hs
@@ -28,9 +28,37 @@ getFileContents path = catch (fmap Just (readFile path)) handler
     handler :: IOException -> IO (Maybe String)
     handler _ = return Nothing
 
+{-
+    | removeLeadingSpaces function
+
+    Removes leading spaces from a string
+-}
 removeLeadingSpaces :: String -> String
 removeLeadingSpaces = dropWhile isSpace
 
+{-  | getNbPixels function
+
+    Gets the number of pixels in the parsed file
+-}
+getNbPixels :: In -> Int -> Int
+getNbPixels (In []) acc = acc
+getNbPixels (In (_:xs)) acc = getNbPixels (In xs) (acc + 1)
+
+{-  | checkNbPixels function
+
+    Checks if the number of pixels is greater than the number of colors
+    Error message if not enough pixels
+-}
+checkNbPixels :: Int -> Int -> IO ()
+checkNbPixels pixels colors'
+    | pixels < colors' = myError "Error:\n\tnot enough pixels" >> return ()
+    | otherwise = return ()
+
+{-
+    | verifyColorValue function
+
+    Verifies if the color value is valid (between 0 and 255)
+-}
 verifyColorValue :: Maybe Color -> Maybe Color
 verifyColorValue Nothing = Nothing
 verifyColorValue (Just (x, y, z))
@@ -38,32 +66,50 @@ verifyColorValue (Just (x, y, z))
         Just(x, y, z)
     | otherwise = Nothing
 
-verifyPoint :: String -> Maybe Point
-verifyPoint str = readMaybe (removeLeadingSpaces str) :: Maybe Point
+{-
+    | returnLine function
 
-verifyColor :: String -> Maybe Color
-verifyColor str =
-    verifyColorValue (readMaybe (removeLeadingSpaces str) :: Maybe Color)
-
+    Returns a line if the point and the color are valid
+-}
 returnLine :: Maybe Point -> Maybe Color -> Maybe Line
 returnLine Nothing _ = Nothing
 returnLine _ Nothing = Nothing
 returnLine (Just point) (Just color) = Just(point, color)
 
+{-
+    | verifyPoint function
+
+    Parses a point and verifies if it is a valid
+-}
+verifyPoint :: String -> Maybe Point
+verifyPoint str = readMaybe (removeLeadingSpaces str) :: Maybe Point
+
+{-
+    | verifyColor function
+
+    Parses a color and verifies if it is a valid
+-}
+verifyColor :: String -> Maybe Color
+verifyColor str =
+    verifyColorValue (readMaybe (removeLeadingSpaces str) :: Maybe Color)
+
+
+{-  | verifyLine function
+
+    Parses a line and verifies if it is a valid
+-}
 verifyLine :: String -> Maybe Line
 verifyLine str =
     returnLine (verifyPoint (tuple !! 0)) (verifyColor (tuple !! 1))
     where
         tuple = split (keepDelimsR $ oneOf ")") str
 
--- ALTERNATIVE TO verifyImg FUNCTION, SAME SPEED
--- verifyLine :: String -> Maybe Line
--- verifyLine str = do
---     let splitList = split (keepDelimsR $ oneOf ")") str
---     case splitList of
---         [point, color, _] -> returnLine (verifyPoint point) (verifyColor color)
---         _ -> Nothing
 
+{-
+    | verifyImg function
+
+    Parses the file content and verifies if it is a valid
+-}
 verifyImg :: [String] -> Maybe [Line] -> Maybe In
 verifyImg (x:xs) (Just in') = case verifyLine x of
     Just line -> verifyImg xs (Just (line : in'))
@@ -71,13 +117,23 @@ verifyImg (x:xs) (Just in') = case verifyLine x of
 verifyImg _ Nothing = Nothing
 verifyImg [] (Just in') = Just (In in')
 
-getNbPixels :: In -> Int -> Int
-getNbPixels (In []) acc = acc
-getNbPixels (In (_:xs)) acc = getNbPixels (In xs) (acc + 1)
+
+{-  | fileParser function
+    
+    Parses the file content
+    Error message if the file is invalid, empty or not found
+-}
+fileParser :: Maybe String -> IO (In)
+fileParser Nothing = myError "Error:\n\tfile not found" >> return (In [])
+fileParser (Just "") = myError "Error:\n\tempty file" >> return (In [])
+fileParser (Just content) = case verifyImg (lines content) (Just []) of
+    Just img -> print img >> return img
+    Nothing -> myError "Error:\n\tinvalid file" >> return (In [])
 
 {-  | launchFile function
 
     Gets the file content and launches the file parser
+    Also saves the number of pixels and the parsed file into conf
 -}
 launchFile :: VerifiedConf -> IO ()
 launchFile oldconf = do
@@ -89,15 +145,11 @@ launchFile oldconf = do
     }
     checkNbPixels (_nbPixels conf) (_nbColors conf)
     return ()
-    
-fileParser :: Maybe String -> IO (In)
-fileParser Nothing = myError "Error:\n\tfile not found" >> return (In [])
-fileParser (Just "") = myError "Error:\n\tempty file" >> return (In [])
-fileParser (Just content) = case verifyImg (lines content) (Just []) of
-    Just img -> print img >> return img
-    Nothing -> myError "Error:\n\tinvalid file" >> return (In [])
 
-checkNbPixels :: Int -> Int -> IO ()
-checkNbPixels nbPixels' nbColors'
-    | nbPixels' < nbColors' = myError "Error:\n\tnot enough pixels" >> return ()
-    | otherwise = return ()
+-- ALTERNATIVE TO verifyImg FUNCTION, SAME SPEED
+-- verifyLine :: String -> Maybe Line
+-- verifyLine str = do
+--     let splitList = split (keepDelimsR $ oneOf ")") str
+--     case splitList of
+--         [point, color, _] -> returnLine (verifyPoint point) (verifyColor color)
+--         _ -> Nothing
